668738875a1d8f33fd2c8054f2ff74b1914e390f
PabasaraPallegama <136107658+PabasaraPallegama@users.noreply.github.com>
2023-07-25 19:11:17 +0530

    2023-07-20 -lab sheet-GUI Programming

---
+++ GUIColorChange/src/main/java/com/mycompany/guicolorchange/GUIColorChange.java
@@ -0,0 +1,10 @@
+package com.mycompany.guicolorchange;
+public class GUIColorChange 
+{
+
+    public static void main(String[] args)
+    {
+        ColorChange cc = new ColorChange();
+        cc.show();
+    }
+}
