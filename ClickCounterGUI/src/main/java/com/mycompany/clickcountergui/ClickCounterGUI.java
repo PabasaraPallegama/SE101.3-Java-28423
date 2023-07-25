b24485e447d34b2ffe39a76dd42e1d6e05a9e008
PabasaraPallegama <136107658+PabasaraPallegama@users.noreply.github.com>
2023-07-25 19:13:52 +0530

    2023-07-20-lab sheet

---
+++ ClickCounterGUI/src/main/java/com/mycompany/clickcountergui/ClickCounterGUI.java
@@ -0,0 +1,10 @@
+package com.mycompany.clickcountergui;
+public class ClickCounterGUI 
+{
+
+    public static void main(String[] args) 
+    {
+        ClickCounter cc = new ClickCounter();
+        cc.show();
+    }
+}
