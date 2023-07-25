ff6be0f0f287417c32ee9abbf3e730287562c7e3
PabasaraPallegama <136107658+PabasaraPallegama@users.noreply.github.com>
2023-07-25 19:14:55 +0530

    2023-07-20-lab sheet

---
+++ BitComplexGUI/src/main/java/com/mycompany/bitcomplexgui/BitComplexGUI.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.bitcomplexgui;
+public class BitComplexGUI 
+{
+
+    public static void main(String[] args) 
+    {
+        BitComplex bc = new BitComplex();
+        bc.show();
+    }
+}
